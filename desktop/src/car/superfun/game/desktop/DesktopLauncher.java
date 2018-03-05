824f12bb484e212c571520dad814dfa9a1be6af2
Paalch <pciverse@stud.ntnu.no>
2018-03-05 21:17:17 +0100

    Init

---
+++ desktop/src/car/superfun/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package car.superfun.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import car.superfun.game.CarSuperFun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CarSuperFun(), config);
+	}
+}
