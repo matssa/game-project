7789d25433f8129890112335d1aaf78c5a5061c7
Kristian Trandem <kkrorkk@gmail.com>
2018-03-13 18:53:44 +0100

    create new map

---
+++ android/assets/tiled_maps/spritesheet_objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spritesheet_objects" tilewidth="130" tileheight="130" tilecount="49" columns="7">
+ <image source="../racing-pack/Spritesheets/spritesheet_objects.png" width="1024" height="1024"/>
+</tileset>
