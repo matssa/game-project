61871491527e0f459da7a07c38d2a8291b321899
Kristian Trandem <kkrorkk@gmail.com>
2018-04-15 23:59:55 +0200

    create new tilesets with a decent structure

---
+++ android/assets/tiles/fenceTiles.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="fenceTiles" tilewidth="256" tileheight="256" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="fence/fenceLargeTurn-0-0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="fence/fenceLargeTurn-0-1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="fence/fenceLargeTurn-1-0.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="fence/fenceSmallTurn.png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="128" source="fence/fenceStraight.png"/>
+ </tile>
+</tileset>
---
+++ android/assets/tiles/roadTiles.tsx
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="roadTiles" tilewidth="256" tileheight="256" tilecount="30" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="78">
+  <image width="256" height="256" source="road/horizontal.png"/>
+ </tile>
+ <tile id="79">
+  <image width="256" height="256" source="road/hugeTurn-0-1.png"/>
+ </tile>
+ <tile id="80">
+  <image width="256" height="256" source="road/hugeTurn-0-2.png"/>
+ </tile>
+ <tile id="81">
+  <image width="256" height="256" source="road/hugeTurn-0-3.png"/>
+ </tile>
+ <tile id="82">
+  <image width="256" height="256" source="road/hugeTurn-1-0.png"/>
+ </tile>
+ <tile id="83">
+  <image width="256" height="256" source="road/hugeTurn-1-1.png"/>
+ </tile>
+ <tile id="84">
+  <image width="256" height="256" source="road/hugeTurn-1-3.png"/>
+ </tile>
+ <tile id="85">
+  <image width="256" height="256" source="road/hugeTurn-2-0.png"/>
+ </tile>
+ <tile id="86">
+  <image width="256" height="256" source="road/hugeTurn-2-2.png"/>
+ </tile>
+ <tile id="87">
+  <image width="256" height="256" source="road/hugeTurn-2-3.png"/>
+ </tile>
+ <tile id="88">
+  <image width="256" height="256" source="road/hugeTurn-3-0.png"/>
+ </tile>
+ <tile id="89">
+  <image width="256" height="256" source="road/hugeTurn-3-1.png"/>
+ </tile>
+ <tile id="90">
+  <image width="256" height="256" source="road/hugeTurn-3-2.png"/>
+ </tile>
+ <tile id="91">
+  <image width="256" height="256" source="road/largeInnerTurn-0-1.png"/>
+ </tile>
+ <tile id="92">
+  <image width="256" height="256" source="road/largeInnerTurn-1-0.png"/>
+ </tile>
+ <tile id="93">
+  <image width="256" height="256" source="road/largeInnerTurn-1-1.png"/>
+ </tile>
+ <tile id="94">
+  <image width="256" height="256" source="road/largeOuterTurn-0-0.png"/>
+ </tile>
+ <tile id="95">
+  <image width="256" height="256" source="road/largeOuterTurn-0-1.png"/>
+ </tile>
+ <tile id="96">
+  <image width="256" height="256" source="road/largeOuterTurn-1-0.png"/>
+ </tile>
+ <tile id="97">
+  <image width="256" height="256" source="road/middle.png"/>
+ </tile>
+ <tile id="98">
+  <image width="256" height="256" source="road/side.png"/>
+ </tile>
+ <tile id="99">
+  <image width="256" height="256" source="road/smallElongatedInnerTurn-0-1.png"/>
+ </tile>
+ <tile id="100">
+  <image width="256" height="256" source="road/smallElongatedInnerTurn-1-0.png"/>
+ </tile>
+ <tile id="101">
+  <image width="256" height="256" source="road/smallElongatedInnerTurn-1-1.png"/>
+ </tile>
+ <tile id="102">
+  <image width="256" height="256" source="road/smallElongatedOuterTurn-0-0.png"/>
+ </tile>
+ <tile id="103">
+  <image width="256" height="256" source="road/smallElongatedOuterTurn-0-1.png"/>
+ </tile>
+ <tile id="104">
+  <image width="256" height="256" source="road/smallElongatedOuterTurn-1-0.png"/>
+ </tile>
+ <tile id="106">
+  <image width="256" height="256" source="road/widener-0-0.png"/>
+ </tile>
+ <tile id="107">
+  <image width="256" height="256" source="road/widener-0-1.png"/>
+ </tile>
+ <tile id="109">
+  <image width="256" height="256" source="road/corner.png"/>
+ </tile>
+</tileset>
