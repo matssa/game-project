ba5011207d5aef36abff72540541e5bd6501e582
matssa <mats.sater@gmail.com>
2018-04-15 23:39:08 +0200

    add a new map

---
+++ android/assets/tiled_maps/spritesheet_tiles_edited.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spritesheet_tiles" tilewidth="128" tileheight="128" tilecount="512" columns="32">
+ <image source="../racing-pack/Spritesheets/spritesheet_tiles.png" width="4096" height="2048"/>
+</tileset>
