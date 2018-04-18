a3c67ad702ca7a1233efb90d0da9109b234db2a0
Kristian Trandem <kkrorkk@gmail.com>
2018-04-18 23:29:26 +0200

    use only one tileset to improve perfomance (they said)

--- android/assets/tiles/overlay.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="overlay" tilewidth="128" tileheight="256" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="128" height="256" source="overlay/goalLine.png"/>
- </tile>
-</tileset>
--- android/assets/tiles/tiles.tsx
+++ android/assets/tiles/tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="roadTiles" tilewidth="256" tileheight="256" tilecount="31" columns="0">
+<tileset name="roadTiles" tilewidth="256" tileheight="256" tilecount="49" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="fence/fenceLargeTurn-0-0.png"/>
@@ -222,5 +222,11 @@
  </tile>
  <tile id="110">
   <image width="256" height="256" source="terrain/green.png"/>
+ </tile>
+ <tile id="111">
+  <image width="256" height="256" source="terrain/grass.png"/>
+ </tile>
+ <tile id="112">
+  <image width="128" height="256" source="overlay/goalLine.png"/>
  </tile>
 </tileset>
